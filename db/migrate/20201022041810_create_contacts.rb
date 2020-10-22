40e1b83a919a619cea64d037252d34de9c2e389b
Franklyn Rodriguez <franklynr92@gmail.com>
2020-10-22 00:31:12 -0400

    adding fields to contact form

--- db/migrate/20201022041810_create_contacts.rb
+++ db/migrate/20201022041810_create_contacts.rb
@@ -3,7 +3,11 @@ class CreateContacts < ActiveRecord::Migration[6.0]
     create_table :contacts do |t|
       t.string :first_name
       t.string :last_name
-      t.string :e
+      t.string :email
+      t.text :message
+      t.number :phone_number
+      t.string :subject
+
 
       t.timestamps
     end
