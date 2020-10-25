5496687cc9858b3d663309ae35e5a830e03cc3be
Franklyn Rodriguez <franklynr92@gmail.com>
2020-10-25 14:13:43 -0400

    removing uncessary routes and views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,12 @@
 class ContactsController < ApplicationController
 
-    def index
-        @contact = Contact.new
-    end
+ 
 
     def new
         @contact = Contact.new
     end
 
-    def show
-    end
+
 
     def create
         @contact = Contact.new
